8026a3cc6e5dc69e14f8633f5f34d15c55dded4b
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-04-30 17:48:03 -0300

    first

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RecentExpenses = () => {
+  return (
+    <View>
+      <Text>Recent Expenses Screen</Text>
+    </View>
+  );
+};
+
+export default RecentExpenses;
+
+const styles = StyleSheet.create({});
