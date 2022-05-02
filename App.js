0a1f65a100b23048be12e023427af87455196ce6
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-05-02 09:06:06 -0300

    navigation

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ManageExpenses from './screens/ManageExpenses';
+import AllExpenses from './screens/AllExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTab.Navigators screenOptions={{}}>
+      <BottomTab.Screen name="AllExpenses" component={AllExpenses} />
+      <BottomTab.Screen name="RecentExpenses" component={RecentExpenses} />
+    </BottomTab.Navigators>
+  );
+};
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
