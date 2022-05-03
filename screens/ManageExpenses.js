b627159a6bd18b5d043f93853927677fa4a7eb0c
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-05-03 12:51:40 -0300

    custom buttons

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Button';
 
 const ManageExpenses = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -13,14 +14,35 @@ const ManageExpenses = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          Confirm
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
             name="trash"
             color={GlobalStyles.colors.error500}
             size={36}
+            onPress={deleteExpenseHandler}
           />
         </View>
       )}
@@ -41,6 +63,15 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     borderTopWidth: 2,
     borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
   },
 });
